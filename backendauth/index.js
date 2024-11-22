be8ad2fc0f2fc005d2815334e330a5a118d0e523
bazedgul1 <bazeed.gul@txend.com>
2024-11-22 18:08:13 +0500

    cors issue resolved

--- backendauth/index.js
+++ backendauth/index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
 import authRoutes from './routes/auth.route.js'
+import cors from 'cors'
 
 
 
 dotenv.config();
-
 const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 
