0175c2dd14f0836b317438703ff42b31c68c36c5
Ibtisam-txd <muhammad.ibtisam@txend.com>
2024-11-22 17:03:03 +0500

    --Update: Bug Resolved for Redirection on Contact page--

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,15 +1,4 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll("header nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      target.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  });
-});
+
 
 // Login Form Validation and Redirect
 document.addEventListener("DOMContentLoaded", () => {
