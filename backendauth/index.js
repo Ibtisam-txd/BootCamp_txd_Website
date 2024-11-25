7c3c0e1637fd91c70bf4d69e54d7e417a5f26148
bazedgul1 <bazeed.gul@txend.com>
2024-11-25 11:12:13 +0500

    local working done -

--- backendauth/index.js
+++ backendauth/index.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500/FrontEnd/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
